78ac27acf052797be46a6276927efc1b2dccdcfe
Roi Katz <kroi444@gmail.com>
2024-04-04 19:54:27 +0300

    Optimized code

--- app/src/main/java/il/co/bringthemhome/ui/ScreenSlidePagerAdapter.kt
+++ app/src/main/java/il/co/bringthemhome/ui/ScreenSlidePagerAdapter.kt
@@ -3,8 +3,6 @@ package il.co.bringthemhome.ui
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import il.co.bringthemhome.AboutFragment
-import il.co.bringthemhome.ui.screens.AllKidnappedFragment
 import il.co.bringthemhome.ui.screens.ActivitiesFragment
 import il.co.bringthemhome.ui.screens.ReleasedFragment
 import il.co.bringthemhome.ui.screens.SearchFragment
@@ -15,13 +13,11 @@ class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
     override fun getItemCount(): Int = NUM_PAGES
 
     override fun createFragment(position: Int): Fragment {
-        // כאן תחזיר את ה-Fragment המתאים לפי המיקום
-        // הנח שיש לך שלושה Fragments שונים שאתה רוצה להציג
         return when (position) {
             0 -> ActivitiesFragment()
             1 -> ReleasedFragment()
             2 -> SearchFragment()
-            else -> AboutFragment()
+            else -> SearchFragment()
         }
     }
 }
